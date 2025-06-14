2e65abdf1a86957ea9f40919d8b1c7ccfc5fdafa
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-06-14 15:30:07 +0200

    modified chart

--- src/app/dashboard/components/chart.tsx
+++ src/app/dashboard/components/chart.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import MinifiedLog from "@/interfaces/minified-log";
-import { BarChart, Bar, Tooltip, XAxis, YAxis, ResponsiveContainer } from "recharts";
+import { XAxis, YAxis, ResponsiveContainer, CartesianGrid, Area, AreaChart } from "recharts";
 
 interface _props {
     logs: MinifiedLog[];
@@ -9,19 +9,32 @@ interface _props {
 
 export default function Chart({ logs }: _props) {
 
-    const chartData = logs.map(log => ({
+    const data = logs.map(log => ({
         ...log,
-        created: new Date(log.created).toLocaleTimeString(),
-    })).reverse()
+        created: new Date(log.created).toLocaleDateString(),
+        quality: log.quality,
+    })).reverse();
 
     return (
-        <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={chartData}>
-                <XAxis dataKey="duration" />
-                <YAxis />
-                <Tooltip />
-                <Bar dataKey="duration" fill="#8884d8" />
-            </BarChart>
+        <ResponsiveContainer
+            width="100%"
+            height={350}>
+            <AreaChart
+                data={data}>
+                <CartesianGrid
+                    stroke="#7d7d7d40"
+                    strokeDasharray="5 5" />
+                <XAxis dataKey="created" />
+                <YAxis dataKey="duration" />
+                <YAxis dataKey="quality" />
+                <Area
+                    animationDuration={2500}
+                    stroke="#ffffff"
+                    fill="#7d7d7d40"
+                    type={"monotone"}
+                    strokeWidth={2}
+                    dataKey="duration" />
+            </AreaChart>
         </ResponsiveContainer>
     );
-}   
+}
--- src/interfaces/minified-log.ts
+++ src/interfaces/minified-log.ts
@@ -1,4 +1,5 @@
 export default interface MinifiedLog {
     created: Date;
     duration: number;
+    quality: number;
 }
