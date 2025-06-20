6a567a1ad7d31f5ff6bc101fd71e77ba841ae78d
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-06-20 15:17:35 +0200

    fixed POST request error

--- src/app/api/log/delete/[id]/route.ts
+++ src/app/api/log/delete/[id]/route.ts
@@ -2,36 +2,43 @@ import database from '@/config/database';
 import getAuth from '@/functions/get-auth';
 import APIResponse from '@/interfaces/api-response';
 import { NextRequest, NextResponse } from 'next/server';
+export async function POST(_request: NextRequest, context: { params: Promise<{ id: string }> }): Promise<NextResponse<APIResponse>> {
+    const params = await context.params;
 
-export async function POST(request: NextRequest, { params }: { params: { id: string } }): Promise<NextResponse<APIResponse>> {
+    const uuid = params.id;
+    console.log(_request.method);
 
     const auth = await getAuth();
-    if (!auth) return NextResponse.json({
-        data: null,
-        message: "Auth failed",
-        status: 403,
-        success: false
-    });
-
-    const { id } = params;
-    if (!id || id.length < 1) return NextResponse.json({
-        data: null,
-        message: "Id malformed",
-        status: 400,
-        success: false
-    });
-
-    try {
-
-        const target = await database.log.findUnique({ where: { id } });
-        if (!target) return NextResponse.json({
+    if (!auth) {
+        return NextResponse.json({
             data: null,
-            message: "Target log not found",
-            status: 404,
+            message: "Auth failed",
+            status: 403,
             success: false
         });
+    }
 
-        await database.log.delete({ where: { id } });
+    if (!uuid || uuid.length < 1) {
+        return NextResponse.json({
+            data: null,
+            message: "Id malformed",
+            status: 400,
+            success: false
+        });
+    }
+
+    try {
+        const target = await database.log.findUnique({ where: { id: uuid } });
+        if (!target) {
+            return NextResponse.json({
+                data: null,
+                message: "Target log not found",
+                status: 404,
+                success: false
+            });
+        }
+
+        await database.log.delete({ where: { id: uuid } });
         return NextResponse.json({
             data: null,
             message: "Log deleted successfully",
