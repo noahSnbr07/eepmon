eceece281701b5db046cf616d7e5ca1b385989f6
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-06-21 12:50:13 +0200

    fixed NaN if empty logs

--- src/app/dashboard/components/recent.tsx
+++ src/app/dashboard/components/recent.tsx
@@ -13,22 +13,28 @@ export default async function Recent({ logs }: _props) {
 
     return (
         <div className="flex flex-col">
-            {logs.slice(0, 5).map((log, _index) =>
-                <div
-                    key={_index}
-                    className="odd:bg-stack first:rounded-t-lg last:rounded-b-lg flex gap-2 py-2 px-4 justify-between items-center">
-                    <b> {getFormattedDuration({ duration: log.duration || 0 })} </b>
-                    <i className="text-sm opacity-50">
-                        {log.created.toLocaleDateString()}
-                    </i>
-                    <MutationButton
-                        endpoint={`/api/log/delete/${log.id}`}
-                        name=""
-                        className="p-1 bg-stack rounded-sm"
-                        icon={<Trash opacity={.5} />}
-                        reload
-                    />
-                </div>
+            {logs.slice(0, 5).map((log, _index) => {
+                const safeDuration = Number(log.duration) || 0;
+                const tempCreatedDate = new Date(log.created);
+                const finalCreatedDate = isNaN(tempCreatedDate.getTime()) ? new Date() : tempCreatedDate;
+                return (
+                    <div
+                        key={_index}
+                        className="odd:bg-stack first:rounded-t-lg last:rounded-b-lg flex gap-2 py-2 px-4 justify-between items-center">
+                        <b> {getFormattedDuration({ duration: safeDuration || 0 })} </b>
+                        <i className="text-sm opacity-50">
+                            {finalCreatedDate.toLocaleDateString()}
+                        </i>
+                        <MutationButton
+                            endpoint={`/api/log/delete/${log.id}`}
+                            name=""
+                            className="p-1 bg-stack rounded-sm"
+                            icon={<Trash opacity={.5} />}
+                            reload
+                        />
+                    </div>
+                )
+            }
             )}
         </div>
     );
