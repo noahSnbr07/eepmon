d8e7905e389e10deadb6ba9cc268122aaf897287
pinguin <noah@eule.fritz.box>
2025-06-22 19:52:52 +0200

    recent logs animated and csr

--- src/app/dashboard/components/recent.tsx
+++ src/app/dashboard/components/recent.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import getFormattedDuration from "@/functions/get-formatted-duration";
 import MutationButton from "@/utils/components/mutation-button";
 import { Log } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { motion } from "framer-motion";
 
 interface _props {
     logs: Log[];
 }
 
-export default async function Recent({ logs }: _props) {
+export default function Recent({ logs }: _props) {
 
     if (logs.length < 1) return <i className="opacity-50"> no logs yet ... </i>
 
@@ -18,7 +21,10 @@ export default async function Recent({ logs }: _props) {
                 const tempCreatedDate = new Date(log.created);
                 const finalCreatedDate = isNaN(tempCreatedDate.getTime()) ? new Date() : tempCreatedDate;
                 return (
-                    <div
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: .25 * _index, type: "keyframes", }}
                         key={_index}
                         className="odd:bg-stack first:rounded-t-lg last:rounded-b-lg flex gap-2 py-2 px-4 justify-between items-center">
                         <b> {getFormattedDuration({ duration: safeDuration || 0 })} </b>
@@ -32,7 +38,7 @@ export default async function Recent({ logs }: _props) {
                             icon={<Trash opacity={.5} />}
                             reload
                         />
-                    </div>
+                    </motion.div>
                 )
             }
             )}
