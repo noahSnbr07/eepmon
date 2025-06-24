3b1aaaccb92b1fa13c7c48d069d309b16df0e882
pinguin <nur.noah.saschenbrecker@gmail.com>
2025-06-24 20:11:43 +0200

    restyled infobox

--- src/app/dashboard/components/archive-state-info.tsx
+++ src/app/dashboard/components/archive-state-info.tsx
@@ -5,8 +5,8 @@ import { Info } from "lucide-react";
 export default function ArchiveStateInfo() {
 
     return (
-        <div className="bg-stack rounded-xl flex gap-4 px-4 py-2">
-            <Info size={16} />
+        <div className="bg-stack items-center rounded-xl flex gap-4 px-4 py-2">
+            <Info size={16} opacity={.5} />
             <b className="text-sm opacity-50">
                 This project has been archived for now.
             </b>
